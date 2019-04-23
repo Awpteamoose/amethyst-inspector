24382950ed115a1d08deb8964ffc63ab69a7b29c
Awpteamoose <awpteamoose@gmail.com>
2019-04-23 14:06:26 +0300

    fix type shadowing in inspector causing syntax errors, add rgba inspector

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use amethyst::{
 	ecs::prelude::*,
 };
 use amethyst_imgui::imgui;
+pub use paste;
 
 #[derive(Default)]
 pub struct InspectorState {
@@ -24,7 +25,7 @@ impl<'s> System<'s> for InspectorHierarchy {
 		amethyst_imgui::with(move |ui| {
 			ui.window(imgui::im_str!("Hierarchy"))
 				.build(move || {
-					fn render_boy (entity: Entity, hierarchy: &amethyst::core::ParentHierarchy, names: &ReadStorage<'_, amethyst::core::Named>, ui: &imgui::Ui<'_>, inspector_state: &mut InspectorState) {
+					fn render_boy(entity: Entity, hierarchy: &amethyst::core::ParentHierarchy, names: &ReadStorage<'_, amethyst::core::Named>, ui: &imgui::Ui<'_>, inspector_state: &mut InspectorState) {
 						let children = hierarchy.children(entity);
 
 						let label: imgui::ImString = if let Some(name) = names.get(entity) {
@@ -68,16 +69,15 @@ pub trait Inspect {
 	fn inspect(&mut self, entity: Entity, ui: &imgui::Ui<'_>);
 }
 
-#[cfg(feature = "named")]
 impl Inspect for Named {
 	fn inspect(&mut self, entity: Entity, ui: &imgui::Ui<'_>) {
 		let mut buf = imgui::ImString::new(self.name.clone());
 		ui.input_text(imgui::im_str!("##named{}{}", entity.id(), entity.gen().id()), &mut buf).resize_buffer(true).build();
 		self.name = std::borrow::Cow::from(String::from(buf.to_str()));
+		ui.separator();
 	}
 }
 
-#[cfg(feature = "transform")]
 impl Inspect for Transform {
 	fn inspect(&mut self, entity: Entity, ui: &imgui::Ui<'_>) {
 		{
@@ -100,36 +100,58 @@ impl Inspect for Transform {
 			ui.drag_float2(imgui::im_str!("scale##transform{}{}", entity.id(), entity.gen().id()), &mut v).speed(0.1).build();
 			self.set_scale(v[0], v[1], 1.);
 		}
+		ui.separator();
+	}
+}
+
+impl Inspect for amethyst::renderer::SpriteRender {
+	fn inspect(&mut self, entity: Entity, ui: &imgui::Ui<'_>) {
+		let mut sprite_number = self.sprite_number as i32;
+		ui.drag_int(imgui::im_str!("sprite_number##sprite_render{}{}", entity.id(), entity.gen().id()), &mut sprite_number).build();
+		self.sprite_number = sprite_number as usize;
+		ui.separator();
+	}
+}
+
+impl Inspect for amethyst::renderer::Rgba {
+	fn inspect(&mut self, entity: Entity, ui: &imgui::Ui<'_>) {
+		use amethyst::renderer::Rgba;
+
+		let mut v: [f32; 4] = [self.0, self.1, self.2, self.3];
+		ui.drag_float4(imgui::im_str!("rgba##rgba{}{}", entity.id(), entity.gen().id()), &mut v).speed(0.1).build();
+		std::mem::replace(self, v.into());
+		ui.separator();
 	}
 }
 
 #[macro_export]
 macro_rules! inspector {
 	($($cmp:ident),+$(,)*) => {
+		#[derive(Default)]
 		#[allow(missing_copy_implementations)]
 		pub struct Inspector;
 		impl<'s> System<'s> for Inspector {
 			type SystemData = (
-				Read<'s, InspectorState>,
+				Write<'s, InspectorState>,
 				$(WriteStorage<'s, $cmp>,)+
-				Entities<'s>,
 			);
 
-			#[allow(non_snake_case)]
-			fn run(&mut self, (inspector_state, $(mut $cmp,)+ _entities): Self::SystemData) {
-				amethyst_imgui::with(move |ui| {
-					ui.window(imgui::im_str!("Inspector"))
-						.size((300.0, 500.0), imgui::ImGuiCond::FirstUseEver)
-						.build(move || {
-							let entity = if let Some(x) = inspector_state.selected { x } else { return; };
-							$(
-								if let Some(cmp) = $cmp.get_mut(entity) {
-									cmp.inspect(entity, ui);
-								}
-								ui.separator();
-							)+
-						});
-				});
+			$crate::paste::item! {
+				#[allow(non_snake_case)]
+				fn run(&mut self, (mut inspector_state, $(mut [<hello $cmp>],)+): Self::SystemData) {
+					amethyst_imgui::with(move |ui| {
+						ui.window(imgui::im_str!("Inspector"))
+							.size((300.0, 500.0), imgui::ImGuiCond::FirstUseEver)
+							.build(move || {
+								let entity = if let Some(x) = inspector_state.selected { x } else { return; };
+								$(
+									if let Some(cmp) = [<hello $cmp>].get_mut(entity) {
+										cmp.inspect(entity, ui, &mut inspector_state);
+									}
+								)+
+							});
+					});
+				}
 			}
 		}
 	};
