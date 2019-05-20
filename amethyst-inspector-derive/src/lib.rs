5dff8591d10cd4a4aa9d25439daf3e8174ee8ea3
Awpteamoose <awpteamoose@gmail.com>
2019-05-20 12:42:21 +0300

    can derive InspectControl on structs

--- amethyst-inspector-derive/src/lib.rs
+++ amethyst-inspector-derive/src/lib.rs
@@ -247,3 +247,113 @@ fn with_component_body(name: &syn::Ident, data: syn::Ident, components: &[syn::P
 		}
 	}
 }
+
+#[proc_macro_derive(InspectControl, attributes(inspect))]
+pub fn derive_inspect_control(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+	let input = parse_macro_input!(input as DeriveInput);
+
+	let name = input.ident;
+	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+	let system_data = match &input.data {
+		Data::Struct(data) => {
+			match &data.fields {
+				Fields::Named(fields) => {
+					fields.named.iter().map(|f| {
+						if FieldArgs::from_field(&f).unwrap().skip { return quote!(); };
+
+						let ty = &f.ty;
+						quote! {<&'control mut #ty as ::amethyst_inspector::InspectControl<'control, 'resource>>::SystemData}
+					}).collect::<Vec<_>>()
+				},
+				_ => unimplemented!(),
+			}
+		},
+		_ => unimplemented!(),
+	};
+
+	let control = match input.data {
+		Data::Struct(data) => {
+			match data.fields {
+				Fields::Named(fields) => {
+					fields.named.iter().enumerate().map(|(i, f)| {
+						let args = FieldArgs::from_field(&f).unwrap();
+						if args.skip { return quote!(); };
+
+						let name = &f.ident;
+
+						// TODO: entities/markers
+						// if !args.with_component.is_empty() {
+						//     return with_component_body(f.ident.as_ref().unwrap(), varname, &args.with_component);
+						// }
+
+						// TODO: more field attrs
+						let null_to = args.null_to.map(|x| quote!(.null_to(#x))).unwrap_or(quote!());
+						let speed = args.speed.map(|x| quote!(.speed(#x))).unwrap_or(quote!());
+
+						let index = syn::Index::from(i);
+						quote! {
+							<&mut f32 as ::amethyst_inspector::InspectControl>::control(&mut self.value.#name)
+								.changed(&mut changed)
+								.data(&mut data.#index)
+								#null_to
+								#speed
+								.label(::amethyst_imgui::imgui::im_str!("{}", stringify!(#name)))
+								.build();
+						}
+					}).collect::<Vec<_>>()
+				},
+				_ => unimplemented!(),
+			}
+		},
+		_ => unimplemented!(),
+	};
+
+
+	let builder = syn::Ident::new(&format!("{}ControlBuilder", name), name.span());
+	let expanded = quote! {
+		pub struct #builder<'control, 'resource: 'control> {
+			pub value: &'control mut #name,
+			pub data: Option<&'control mut <&'control mut #name as ::amethyst_inspector::InspectControl<'control, 'resource>>::SystemData>,
+			pub label: Option<&'control ::amethyst_imgui::imgui::ImStr>,
+			pub changed: Option<&'control mut bool>,
+		}
+
+		impl<'control, 'resource: 'control> #impl_generics ::amethyst_inspector::InspectControl<'control, 'resource> for &'control mut #name #ty_generics #where_clause {
+			type SystemData = (#(#system_data),*);
+			type Builder = #builder<'control, 'resource>;
+		}
+
+		impl<'control, 'resource: 'control> ::amethyst_inspector::InspectControlBuilder<'control, 'resource, &'control mut #name> for #builder<'control, 'resource> {
+			fn new(value: &'control mut #name) -> Self {
+				Self { value, label: None, changed: None, data: None }
+			}
+			fn label(mut self, label: &'control imgui::ImStr) -> Self {
+				self.label = Some(label);
+				self
+			}
+			fn changed(mut self, changed: &'control mut bool) -> Self {
+				self.changed = Some(changed);
+				self
+			}
+			fn data(mut self, data: &'control mut <&'control mut #name as ::amethyst_inspector::InspectControl<'control, 'resource>>::SystemData) -> Self {
+				self.data = Some(data);
+				self
+			}
+			fn build(mut self) {
+				let mut changed = false;
+				let mut data = self.data.take().unwrap();
+
+				::amethyst_imgui::with(|ui| {
+					ui.tree_node(::amethyst_imgui::imgui::im_str!("{}", stringify!(#name))).selected(true).build(|| {
+						#(#control)*
+					});
+				});
+
+				if let Some(x) = self.changed { *x = *x || changed };
+			}
+		}
+	};
+
+	proc_macro::TokenStream::from(expanded)
+}
